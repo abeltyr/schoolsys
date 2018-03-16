04e7efae3e6e6d0b355ae43c6643101385bdee74
Abel <abeltyr@gmail.com>
2018-03-16 14:47:37 +0000

    first commit

---
+++ database/migrations/2018_03_16_143320_create_ranks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRanksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ranks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('student_id');
+            $table->string('name');
+            $table->integer('rank');
+            $table->integer('per_by_cla');
+            $table->integer('per_by_bath');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ranks');
+    }
+}
---
+++ database/migrations/2018_03_16_143825_create_lowsubs_table.php
@@ -0,0 +1,63 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLowsubsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lowsubs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('student_id');
+            $table->string('name');
+            $table->string('semister');
+            $table->string('subj1')->default('0');
+            $table->string('subj2')->default('0');
+            $table->string('subj3')->default('0');
+            $table->string('subj4')->default('0');
+            $table->string('subj5')->default('0');
+            $table->string('subj6')->default('0');
+            $table->string('subj7')->default('0');
+            $table->string('subj8')->default('0');
+            $table->string('subj9')->default('0');
+            $table->string('subj10')->default('0');
+            $table->string('subj11')->default('0');
+            $table->string('subj12')->default('0');
+            $table->string('subj13')->default('0');
+            $table->string('subj14')->default('0');
+            $table->string('subj15')->default('0');
+            $table->string('subj16')->default('0');
+            $table->string('subj17')->default('0');
+            $table->string('subj19')->default('0');
+            $table->string('subj20')->default('0');
+            $table->string('subj21')->default('0');
+            $table->string('subj22')->default('0');
+            $table->string('subj23')->default('0');
+            $table->string('subj24')->default('0');
+            $table->string('subj25')->default('0');
+            $table->string('subj26')->default('0');
+            $table->string('subj27')->default('0');
+            $table->string('subj28')->default('0');
+            $table->string('subj29')->default('0');
+            $table->string('subj30')->default('0');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lowsubs');
+    }
+}
