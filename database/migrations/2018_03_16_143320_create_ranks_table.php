007fe204d5328c190ac9174ee987ffd9b7c15ecc
Abel <abeltyr@gmail.com>
2018-03-16 15:11:59 +0000

    first commit

--- database/migrations/2018_03_16_143320_create_ranks_table.php
+++ database/migrations/2018_03_16_143320_create_ranks_table.php
@@ -18,6 +18,7 @@ class CreateRanksTable extends Migration
             $table->string('student_id');
             $table->string('name');
             $table->integer('rank');
+            $table->integer('semister');
             $table->integer('per_by_cla');
             $table->integer('per_by_bath');
             $table->timestamps();
